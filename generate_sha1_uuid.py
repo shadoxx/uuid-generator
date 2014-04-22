ac0f458d55eff5e05cb1f91f8efd8372deb69b56
Brandon Wooldridge <brandon@navi.shadoxx.lan>
2014-04-22 01:01:12 -0400

    fixed bug

--- generate_sha1_uuid.py
+++ generate_sha1_uuid.py
@@ -1,16 +1,36 @@
-import hashlib, os
+import hashlib, os, error_reporter
 from datetime import datetime
+from sys import exit
+
+
+## TODO : - not yet implemented, ! needs work, * feature implemented
+# * validate the user input in the function, return status code indicated success or failure
+# - validate our entropy pool for salt generation, wait until enough entropy is present
+# - write input santization function (convert to all lowercase, ensure alphanumeric, strip whitespace)
+# - add command line functionality
+
+# author: shadoxx - date: april 21, 2014
+################################################################################
 
 ################################################################################
 
 def generate_sha1_uuid( user_common_name, current_time, salt ):
-    """ Generate a UUID using a salt and the provided information. Requires 'import hashlib'. """
-    uuid = hashlib.sha1( str(salt) + str(user_common_name) + str(current_time) )
-    return str( uuid.hexdigest() )
+    """ Generate a UUID using a salt and the provided information. Requires 'import hashlib'. \
+        Return '1' if username passed into function isn't alphanumeric after lower() and strip().
+    """
+    user_common_name = user_common_name.lower()
+    user_common_name = user_common_name.strip()
+
+    if user_common_name.isalnum():
+        uuid = hashlib.sha1( str(current_time) + str(salt) + str(user_common_name) )
+        return str( uuid.hexdigest() )
+    else:
+        return 1
 
 ################################################################################
 
-print "uuid_gen - for creating unique sha1 based user ids\n"
+print "uuid_gen - for creating cryptographically* secure unique sha1 based user ids\n"
+logger = error_reporter.ErrorReporter()    # initialize our error reporting library
 
 uuid_metadata = {
     'name' : None,
@@ -25,5 +45,9 @@ uuid_metadata['time'] = datetime.strftime( datetime.now(), "%Y%m%d%H%M.%S.%f" )
 uuid_metadata['salt'] = os.urandom(16).encode('base_64')
 uuid_metadata['uuid'] = generate_sha1_uuid( uuid_metadata['name'], uuid_metadata['time'], uuid_metadata['salt'] )
 
-print "    uuid: %s\n\nGenerated with the following data: \ntime: %s\nsalt: %s" % \
-        ( uuid_metadata['uuid'], uuid_metadata['time'], uuid_metadata['salt'] )
+if uuid_metadata['uuid'] == 1:
+    print logger.return_formatted_status('ERROR_BAD_USER_INPUT', "usernames must be alphanumic. no symbols or special characters allowed.")
+    exit()
+else:
+    print "    uuid: %s\n\nGenerated with the following data: \ntime: %s\nsalt: %s" % \
+            ( uuid_metadata['uuid'], uuid_metadata['time'], uuid_metadata['salt'] )
